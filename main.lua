c7c6fa615a0fa38cd2a5c8746fbe8b5fb979d465
Carlos Mattoso <calmattoso@hotmail.com>
2011-04-09 23:11:32 -0300

    New version

--- main.lua
+++ main.lua
@@ -1,82 +1,64 @@
-function init_map()
-	local cor = 0
-	local current_line = 0
-	
-	for i=0,1200 do
-		if math.floor(i/40) ~= current_line then
-			cor = (cor == 0 and 1) or 0
-			current_line = current_line + 1
+require 'objects'
+
+map = {
+	register = {},
+	init = function(obj)
+		for i=1,20 do
+			obj.register[i] = 0
 		end
-	
-		if i % 2 == 0 then
-			map[i] = cor
-		else
-			map[i] = (cor == 0 and 1) or 0
-		end		
-	end	
-end
-
-function gen_car(_x, _y, _width, _height, _speedX, _speedY, _color, _dir)
-	car = {x = _x, y = _y, height = _height, width = _width, speedX = _speedX, speedY = _speedY, color=_color, dir = _dir,
-		move = function(car)
-			if car.dir == "right" then
-				car.x = car.x + car.speedX
-			elseif car.dir == "left" then 
-				car.x = car.x - car.speedX
-			elseif car.dir == "up" then
-				car.y = car.y - car.speedY
-			elseif car.dir == "down" then
-				car.y = car.y + car.speedY
-			end
-		end,
-		draw = function(car)
-			love.graphics.setColor(car.color)
-			love.graphics.rectangle("fill", car.x, car.y, car.width, car.height)
-		end,
-		setDir = function(car, new_dir)
-			car.dir = new_dir
-		end	
-	}
-	
-	return car
-end
-
-function love.load()
-	-- Criação do mapa
-	map = {}
-	init_map()
-	
-	car = gen_car(0,0,20,20,20,20,{255,0,0},"right")
-end
-
-function love.update(dt)
-	car:move()
-end
-
-function love.keypressed(key)
-	if key == "left" then
-		car:setDir("left")
-	elseif key == "right" then
-		car:setDir("right")
-	elseif key == "up" then
-		car:setDir("up")
-	elseif key == "down" then
-		car:setDir("down")
+	end,
+	draw = function(obj)
+		for i=1,40 do
+			love.graphics.line(i*20, 0, i*20, 600)
+			love.graphics.line(0, i*20, 800, i*20)
+		end
+	end,
+	co = coroutine.create(function(obj)
+		player = await({"moved"})
+		--obj.register		
 	end
+	)
+	
+
+}
+cars = {}
+
+function drawCar(car)
+	love.graphics.setColor(car.color)
+	love.graphics.rectangle("fill", car.x, car.y, 20, 20) 	
+	love.graphics.setColor(255,255,255)
 end
 
-function love.draw()
-	-- Desenha o mapa
-	for i=0,1200 do
-		--print(map[i])
-		--love.timer.sleep(1000)
-		if map[i] == 0 then
-			love.graphics.setColor(255,255,255)
-			love.graphics.rectangle("fill", 20*(i%40), math.floor(i/40)*20, 20, 20)			
-		end
-		love.graphics.setColor(0,0,0);
-	end	
-	car:draw()
-	
-	love.timer.sleep(100)
+function updateCar(car)
+	local dt=await({"dt"})
+	print(car.direction)
+	if(car.direction == "right") then car.x = car.x + car.speed * dt end
+	if(car.direction == "left") then car.x = car.x - car.speed * dt end
+	if(car.direction == "up") then car.y = car.y - car.speed * dt end
+	if(car.direction == "down") then car.y = car.y + car.speed * dt end
 end
+
+function createCar(x, y, speed, color,direction)	
+	return {x = x, y = y, speed = speed, color = color, direction = direction,
+	 draw = drawCar,
+	 co = coroutine.create(updateCar)
+	}
+end
+map:init()
+
+local cars_info = {
+	{x = 200, y = 300, color = {255,0,0,127}, direction = "right"},
+	{x = 600, y = 300, color = {0,0,255,127}, direction = "left"}
+}
+
+for i=1,2 do
+	cars[#cars + 1] = (createCar(cars_info[i].x, cars_info[i].y, 20, cars_info[i].color, cars_info[i].direction))
+end
+
+
+draws.insert(map, 0)
+for i=1,2 do
+	draws.insert(cars[i], 1)
+	anims.insert(cars[i])
+end
+
